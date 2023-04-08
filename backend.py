d1ef8a83d4245c643810e66e5dacfb3c63d404d1
Zaid Jamal <zaid323918@gmail.com>
2023-04-08 17:09:42 -0500

    assigning

--- backend.py
+++ backend.py
@@ -6,6 +6,14 @@ from dotenv import load_dotenv, find_dotenv
 
 app = flask.Flask(__name__)
 
+#Dullah
+def vision_api():
+    return
+
+#Zach
+def nyt_api():
+    return
+
 @app.route('/')
 def main():
     return flask.render_template('index.html')
