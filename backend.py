1046a70f57350c9e393b6a6712a4fccb93b6c418
Abdullah <aai27@txstate.edu>
2023-04-08 18:23:04 -0500

    Added google api code, needs to be tested

--- backend.py
+++ backend.py
@@ -1,13 +1,40 @@
 import os
+import io
 import requests
 import json
 import flask
 from dotenv import load_dotenv, find_dotenv
 
+from google.cloud import vision_v1
+from google.cloud.vision_v1 import enums
+from google.oauth2 import service_account
+
 app = flask.Flask(__name__)
 
 #Dullah
-def vision_api():
+def detect_text(file_path):
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/path/to/api_key.json'
+
+    credentials = service_account.Credentials.from_service_account_file('/path/to/api_key.json')
+
+    client = vision_v1.ImageAnnotatorClient(credentials=credentials)
+
+    with io.open(file_path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision_v1.types.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    print('Texts:')
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
     return
 
 #Zach
@@ -16,6 +43,7 @@ def nyt_api():
 
 @app.route('/')
 def main():
+    detect_text('/path/to/image.jpg')
     return flask.render_template('index.html')
 
 app.run()
